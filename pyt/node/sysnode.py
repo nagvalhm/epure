13c57e7f72a8e71a5770f1adfc4efed3a24ba030
Павел Пичугин <pavelpichugin28@gmail.com>
2021-11-03 17:26:39 +0300

    added node.sysnode.put method

--- pyt/node/sysnode.py
+++ pyt/node/sysnode.py
@@ -1,6 +1,7 @@
-
+import os
 from .node import Node
-
+from pathlib import Path
+import re
 
 class SysNode(Node):
     _instance = None
@@ -9,3 +10,20 @@ class SysNode(Node):
         if not cls._instance:
             cls._instance = super().__new__(cls)
         return cls._instance
+    
+    # def put(self, path=None, node=None)
+    def put(path):
+        # if not path:
+        #     if not self.path:
+        #         self.path = SysNode.heap = 'config'
+        #     if not self.name:
+        #         self.name = re.sub(r'(?<!^)(?=[A-Z])', '_', type(self).__name__).lower  #to snake_case
+        #     path = Path(self.path + "/" + self.name)
+        if not os.path.exists(path):
+            path_dir = os.path.dirname(path)
+            if not os.path.exists(path_dir):
+                os.mkdir(path_dir)
+            with Path(path).open("w") as f:
+                f.write('writing')
+        return path
+
--- pyt/tests/test_node.py
+++ pyt/tests/test_node.py
@@ -1,3 +1,4 @@
+from os import path, putenv
 import pytest
 from ..node import *
 import sys
@@ -11,3 +12,9 @@ def test_node_sysnode_new():
     ins1 = SysNode()
     ins2 = SysNode()
     assert ins1 is ins2
+
+def test_node_sysnode_put():
+    path = "config/text.txt"
+    SysNode.put(path)
+    assert os.path.exists(path) 
+    return SysNode.put
