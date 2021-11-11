83f1689f3b65816b39932c94a57c52062859913e
nagvalhm <nagvalhm@yandex.ru>
2021-11-11 12:37:13 +0300

    Proto works with no arguments init

--- pyt/epure/proto.py
+++ pyt/epure/proto.py
@@ -1,19 +1,87 @@
 from __future__ import annotations
-from typing import Any
+from abc import abstractmethod, ABCMeta
+from typing import *
 import re
 
 
+class MultipleInheritanceError(Exception):
+    pass
 
-class Proto:
+class ProtoMeta(type):
+
+    def __new__(mcl, name:str, bases:Tuple[type], namespace: dict[str, Any]) -> ProtoMeta:
+        res_cls = super().__new__(mcl, name, bases, namespace)
+
+        if name == 'Proto':
+            return res_cls
+
+        try:
+            res_cls.proto_cls
+        except MultipleInheritanceError:
+            del res_cls
+            raise
+
+        return res_cls
+
+    @property
+    def proto_cls(cls) -> ProtoMeta:
+        bases = cls.__bases__
+        protos = list(filter(lambda base: issubclass(base, Proto), bases))
+        if len(protos) > 1:
+            raise MultipleInheritanceError(f'{cls.__name__} has more than one Proto ancestor: {protos}')
+        
+        if len(protos) == 0:
+            raise Exception()
+        res = protos[0]
+        if not isinstance(res, ProtoMeta):
+            raise Exception('res must be Proto')
+
+        return res
+
+
+
+class Proto(metaclass=ProtoMeta):
     __proto__:Proto
+    __abstractclass__ = 1
+    
 
+    def __new__(cls: ProtoMeta, *args: Any, **kwargs: Any) -> Proto:
+        print(f'{cls.__name__} new called')
+        res = super(type, cls).__new__(cls)
+        if not isinstance(res, Proto):
+            raise
+        # proto = res.proto()
+
+        return res
+
+    # @property
+    def proto(self, *args: Any, **kwargs: Any) -> Proto:
+        self_cls = type(self)
+        if not hasattr(self, '__proto__'):
+            proto_cls = self_cls.proto_cls
+            if '__abstractclass__' in vars(proto_cls).keys():
+                return None
+            self.__proto__ = proto_cls(*args, **kwargs)
+        return self.__proto__
+
+    
     def __setattr__(self, name: str, value: Any) -> None:
         self_cls = type(self)
         if not self_cls.is_special_attr(name):
             return super().__setattr__(name, value)
 
-        if not hasattr(self_cls, name):
+        if name in vars(self_cls).keys():
+            print(f'{self_cls.__name__} set val for {name} = {value}')
+            return super().__setattr__(name, value)
+
+
+        self_proto = self.proto()
+        if self_proto:
+            setattr(self_proto, name, value)
+        else:
             raise AttributeError(f'{type(self)} object has no attribute {name}')
+
+
 
     def __getattribute__(self, name: str) -> Any:
         self_cls = type(self)
@@ -23,8 +91,10 @@ class Proto:
         if not hasattr(self_cls, name):
             raise AttributeError(f'{type(self)} object has no attribute {name}')
 
-    @staticmethod
-    def is_special_attr(name: str) -> bool:
+
+
+    @classmethod
+    def is_special_attr(cls, name: str) -> bool:
         pattern = re.compile(r"___.*___")
         res = bool(pattern.match(name))
         if not res:
--- pyt/tests/test_proto.py
+++ pyt/tests/test_proto.py
@@ -2,16 +2,51 @@
 import pytest
 from ..epure.proto import *
 
-class Cap(Proto):
-    not_special_cap:str = None
+class Container(Proto):
+    ___special_container___ = None
 
+    # def __init__(self, b) -> None:
+    #     super().__init__()
+
+class Сow(Proto):
+    pass
+
+class Cap(Container):
+    not_special_cap:str = None
+    not_inited_field:str
+    ___special_cap___ = None
+
+    # def __init__(self, b) -> None:
+    #     super().__init__(b)
+
+    # def __new__(cls: ProtoMeta, *args: Any, **kwargs: Any) -> Proto:
+    #     return super().__new__(*args, **kwargs)
+
+class BarStuf:
+    pass
 
 class Shaker(Cap):
     not_special_shaker:str
+    ___special_shaker___ = None
+
+
+
+    # def __init__(self, b) -> None:
+    #     pass# super().__init__(b)
+
+
 
 @pytest.fixture
 def proto():
-    return Shaker()
+    res = Shaker(11)
+    return res
+
+def test_proto_new_multiple_inheritance():
+    with pytest.raises(MultipleInheritanceError):
+        class MilkShaker(Сow, Shaker):
+            pass
+
+# def test_proto_inheritance():
 
 def test_proto_setattr_not_special_attr(proto, capsys):
     proto.a = 1    
@@ -26,11 +61,28 @@ def test_proto_getattribute_not_special_attr(proto, capsys):
     captured = capsys.readouterr()
     assert f'not_special_cap is not special\n' == captured.out
 
+
+
+#####################################################
+def test_proto_setattr(proto, capsys):
+    proto.___special_container___ = 1
+    proto.___special_cap___ = 1
+    proto.___special_shaker___ = 1
+
+    captured = capsys.readouterr()
+    res = captured.out
+    print(res)
+    assert f'Container set val for ___special_container___ = 1' in res
+    assert f'Cap set val for ___special_cap___ = 1' in res
+    assert f'Shaker set val for ___special_shaker___ = 1' in res
+    # assert len(res) < 150
+    
+
 def test_proto_setattr_error(proto, capsys):
     with pytest.raises(AttributeError):
         proto.___not_exist___ = 123
 
     
-def test_proto_getattribute_error(proto, capsys):
-    with pytest.raises(AttributeError):
-        a = proto.___not_exist___
+# def test_proto_getattribute_error(proto, capsys):
+#     with pytest.raises(AttributeError):
+#         a = proto.___not_exist___
