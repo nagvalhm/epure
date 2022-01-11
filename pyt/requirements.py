e8745ebf32a0f488d1d002de511d77c1c745dcc6
nagvalhm <nagvalhm@yandex.ru>
2022-01-11 17:04:41 +0300

    minor

--- pyt/requirements.py
+++ pyt/requirements.py
@@ -2,9 +2,10 @@
 import subprocess
 
 def exec_shell_cmd(cmd):
-    return subprocess.call(cmd.split(), shell=True)
+    return subprocess.call(cmd.split())
+    # return subprocess.call(cmd.split(), shell=True)
 
-exec_shell_cmd("pip install --upgrade pip")
-exec_shell_cmd("pip install -r pyt/requirements.txt")
+exec_shell_cmd("pyt//env39//Scripts//pip.exe install --upgrade pip")
+exec_shell_cmd("pyt//env39//Scripts//pip.exe install -r pyt/requirements.txt")
 exec_shell_cmd("pre-commit install")
 # exec_shell_cmd("pre-commit uninstall")
