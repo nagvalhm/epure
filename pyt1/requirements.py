9af1416001652d63fdcb8b2eda4e0af631cd641f
nagvalhm <nagvalhm@yandex.ru>
2022-06-27 17:49:33 +0300

    created structure of pyt1 proj

---
+++ pyt1/requirements.py
@@ -0,0 +1,11 @@
+# type: ignore
+import subprocess
+
+def exec_shell_cmd(cmd):
+    return subprocess.call(cmd.split())
+    # return subprocess.call(cmd.split(), shell=True)
+
+exec_shell_cmd("pyt1//env310//Scripts//pip.exe install --upgrade pip")
+exec_shell_cmd("pyt1//env310//Scripts//pip.exe install -r pyt1/requirements.txt")
+exec_shell_cmd("pre-commit install")
+# exec_shell_cmd("pre-commit uninstall")
