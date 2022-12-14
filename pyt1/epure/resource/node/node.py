7586035978b0d39b55216c066b06e154e5d3d123
pavel <pavelpichugin28@gmail.com>
2022-12-15 01:15:30 +0400

    added Node.from_dict()

--- pyt1/epure/resource/node/node.py
+++ pyt1/epure/resource/node/node.py
@@ -2,6 +2,7 @@ from uuid import UUID
 from ..savable import Savable
 from ..resource import Resource
 from ...errors import ResourceException
+from typing import Any, Dict
 
 class Node(Savable):
 
@@ -12,6 +13,10 @@ class Node(Savable):
         if node_id != None:
             self.node_id = node_id
         super().__init__(name, namespace, resource)
+
+    @classmethod
+    def from_dict(_cls, _dict:Dict[str, Any])->object:
+        raise NotImplementedError
 
     def save(self, asynch:bool=False):
         if not hasattr(self, 'resource'):
@@ -41,5 +46,20 @@ class TableNode(Node):
     def table(self) -> Savable:
         return self.resource
 
-    __exclude__:list = Node.__exclude__ + ['table', 'db']
+    @classmethod
+    def from_dict(_cls, _dict:Dict[str, Any])->object:
 
+        # instance = _cls.__call__()
+        instance = _cls()
+        
+        for field_name, val in _dict.items():
+            if field_name in instance.annotations:
+                _type = instance.annotations[field_name]
+                if isinstance(_type, Savable):
+                    val = _type(val)
+                setattr(instance, field_name, val)
+
+
+        return instance
+
+    __exclude__:list = Node.__exclude__ + ['table', 'db']
