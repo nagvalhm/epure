9b4e140816011e111c8a8a77eed02459d209f97b
nagvalhm <nagvalhm@yandex.ru>
2024-02-04 21:57:46 +0300

    moved method join from model to table, changed model to table within join logic

--- pyt1/epure/resource/join_resource/join_resource.py
+++ pyt1/epure/resource/join_resource/join_resource.py
@@ -2,32 +2,35 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from ...parser.inspect_parser.model import Model
+    from ..db.table import Table
     # from ...resource.db.table import Table
 
 class Join:
-    model:Model
+    table:Table
     on_clause:str
     join_type:str
     alias:str
 
-    def __init__(self, model:Model, on_clause:str, join_type:str="LEFT", alias:str="") -> None:
-        self.model = model
+    def __init__(self, table:Table, on_clause:str, join_type:str="LEFT", alias:str="") -> None:
+        self.table = table
         self.on_clause = on_clause
         self.join_type = join_type
         self.alias = alias
         
 class JoinResource:
-    model:Model
+    table:Table
     joins:list[Join]
 
-    def __init__(self, model:Model) -> None:
+    def __init__(self, table:Table) -> None:
         self.joins = []
-        self.model = model
+        self.table = table
 
-    def join(self, joined_model:Model, on_clause:str, join_type:str="LEFT", alias:str="") -> None:
-        join = Join(joined_model, on_clause, join_type, alias)
-        self.joins.append(join)
+    def join(self, table:Table, on_clause:str, join_type:str="LEFT", alias:str="") -> None:
+        join = Join(table, on_clause, join_type, alias)
+        res = JoinResource(self.table)
+        res.joins = self.joins.copy()
+        res.joins.append(join)
+        return res
 
     def read(self, *args, **kwargs):
 
@@ -42,7 +45,7 @@ class JoinResource:
             where_clause = args[0]
 
         if len(args) <= 1:
-            header = [self.model]
-            header = header + [join.model for join in self.joins]
+            header = [self.table]
+            header = header + [join.table for join in self.joins]
 
-        return self.model.__table__.read(header, self.joins, where_clause, **kwargs)
+        return self.table.read(header, self.joins, where_clause, **kwargs)
